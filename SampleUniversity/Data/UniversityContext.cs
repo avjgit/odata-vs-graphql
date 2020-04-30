34171c87a69abf64160113b64493cdee28c8c622
Jurčenoks, Andrejs <andrejs.jurcenoks@if.lv>
2020-04-30 16:47:58 +0300

    scaffolded views

---
+++ SampleUniversity/Data/UniversityContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SampleUniversity.Model;
+
+namespace SampleUniversity.Data
+{
+    public class UniversityContext : DbContext
+    {
+        public UniversityContext (DbContextOptions<UniversityContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SampleUniversity.Model.Student> Student { get; set; }
+    }
+}
---
+++ SampleUniversity/Pages/Students/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SampleUniversity.Data;
+using SampleUniversity.Model;
+
+namespace SampleUniversity.Pages.Students
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly SampleUniversity.Data.UniversityContext _context;
+
+        public DetailsModel(SampleUniversity.Data.UniversityContext context)
+        {
+            _context = context;
+        }
+
+        public Student Student { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Student = await _context.Student.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Student == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
