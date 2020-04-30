7ed3c4aae97e41ea7c52a91695d6c85ec3faa11a
Jurčenoks, Andrejs <andrejs.jurcenoks@if.lv>
2020-04-30 17:44:47 +0300

    scaffold Courses

---
+++ SampleUniversity/Pages/Courses/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SampleUniversity.Data;
+using SampleUniversity.Model;
+
+namespace SampleUniversity.Pages.Courses
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly SampleUniversity.Data.UniversityContext _context;
+
+        public DetailsModel(SampleUniversity.Data.UniversityContext context)
+        {
+            _context = context;
+        }
+
+        public Course Course { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Course = await _context.Courses.FirstOrDefaultAsync(m => m.CourseID == id);
+
+            if (Course == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ SampleUniversity/Pages/Courses/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SampleUniversity.Data;
+using SampleUniversity.Model;
+
+namespace SampleUniversity.Pages.Courses
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SampleUniversity.Data.UniversityContext _context;
+
+        public IndexModel(SampleUniversity.Data.UniversityContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Course> Course { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Course = await _context.Courses.ToListAsync();
+        }
+    }
+}
