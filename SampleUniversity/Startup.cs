9449996f25f8102e3ebb260112379e7c568a13dc
Jurčenoks, Andrejs <andrejs.jurcenoks@if.lv>
2020-05-02 07:34:04 +0300

    cleanup

--- SampleUniversity/Startup.cs
+++ SampleUniversity/Startup.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using SampleUniversity.Data;
 using Microsoft.AspNet.OData.Extensions;
@@ -18,14 +13,9 @@ namespace SampleUniversity
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
+        public Startup(IConfiguration configuration) => Configuration = configuration;
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
@@ -41,22 +31,9 @@ namespace SampleUniversity
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
@@ -70,9 +47,9 @@ namespace SampleUniversity
                 routeBuilder.Expand().Select().OrderBy().Filter();
             });
 
-            var builder = new ODataConventionModelBuilder(app.ApplicationServices);
+            //var builder = new ODataConventionModelBuilder(app.ApplicationServices);
 
-            builder.EntitySet<Student>("Students");
+            //builder.EntitySet<Student>("Students");
         }
     }
 }
